3e9c91f7447513669faad4d8707857857b2dba35
Kyle Flores <floreskyle93@yahoo.com>
2024-07-09 13:54:36 -0400

    Online Schema Repairs

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,5 @@
 from .db import db
 from .user import User
 from .db import environment, SCHEMA
-from .products import Product,ProductTag,ProductImage, Tags
+from .product import Product,ProductTag,ProductImage, Tags
 from .article import Article, ArticleTag
--- app/models/article.py
+++ app/models/article.py
@@ -1,5 +1,5 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
-from .products import Tags
+from .product import Tags
 
 class Article(db.Model):
     __tablename__ = 'articles'
